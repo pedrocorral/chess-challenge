4e68ea708320bf0e710339b5b57673916628cb6c
pedrocorral <Knight@RedBat.obdin.com>
2015-11-01 21:15:47 +0100

    A few more convincing tests

--- src/test/scala/chess/challenge/test/Distributions.scala
+++ src/test/scala/chess/challenge/test/Distributions.scala
@@ -4,9 +4,8 @@ package chess.challenge.test
 import chess.challenge._
 
 import org.scalatest._
-import Matchers._
 
-class Distributions extends FlatSpec {
+class Distributions extends FlatSpec with Inspectors with Matchers {
 
   lazy val foldResults = (distributions: List[Distribution], distribution: Distribution) => distribution :: distributions
 
@@ -55,6 +54,28 @@ class Distributions extends FlatSpec {
       piece0 <- pieces
       piece1 <- pieces ; if piece0 != piece1
     } grid2x2( piece0 :: piece1 :: Nil, Nil, foldResults ).length shouldEqual validDistributions( piece0, piece1 )
+  }
+
+  "3x3 distribution" should "be created for two kings and one rook" in {
+    val xs = Grid( 3, 3 )( "KRK", Nil, foldResults )
+    xs.length shouldEqual 4
+    exactly( 1, xs ) should contain only ( (0,2,King), (0,0,King), (2,1,Rook) )
+    exactly( 1, xs ) should contain only ( (2,0,King), (0,0,King), (1,2,Rook) )
+    exactly( 1, xs ) should contain only ( (2,2,King), (0,2,King), (1,0,Rook) )
+    exactly( 1, xs ) should contain only ( (2,2,King), (2,0,King), (0,1,Rook) )
+  }
+
+  "4x4 distribution" should "be created for two rooks and 4 knights" in {
+    val xs = Grid( 4, 4 )( "NRNNRN", Nil, foldResults )
+    xs.length shouldEqual 8
+    exactly( 1, xs ) should contain only ( (0,1,Knight), (0,3,Knight), (1,2,Rook), (2,1,Knight), (2,3,Knight), (3,0,Rook) )
+    exactly( 1, xs ) should contain only ( (0,1,Knight), (0,3,Knight), (1,0,Rook), (2,1,Knight), (2,3,Knight), (3,2,Rook) )
+    exactly( 1, xs ) should contain only ( (0,0,Rook), (1,1,Knight), (1,3,Knight), (2,2,Rook), (3,1,Knight), (3,3,Knight) )
+    exactly( 1, xs ) should contain only ( (0,2,Rook), (1,1,Knight), (1,3,Knight), (2,0,Rook), (3,1,Knight), (3,3,Knight) )
+    exactly( 1, xs ) should contain only ( (0,1,Rook), (1,0,Knight), (1,2,Knight), (2,3,Rook), (3,0,Knight), (3,2,Knight) )
+    exactly( 1, xs ) should contain only ( (0,3,Rook), (1,0,Knight), (1,2,Knight), (2,1,Rook), (3,0,Knight), (3,2,Knight) )
+    exactly( 1, xs ) should contain only ( (0,0,Knight), (0,2,Knight), (1,3,Rook), (2,0,Knight), (2,2,Knight), (3,1,Rook) )
+    exactly( 1, xs ) should contain only ( (0,0,Knight), (0,2,Knight), (1,1,Rook), (2,0,Knight), (2,2,Knight), (3,3,Rook) )
   }
 
 }
